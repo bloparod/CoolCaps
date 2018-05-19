2b3b92cc0caff14879ba6fe463bb8762ebcb0597
Pablo Rodriguez <prodriguez@hexacta.com>
2018-05-18 22:39:04 -0300

    Se borró el footer del layout. Se agregó un MediaController básico.

---
+++ Controllers/MediaController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CoolCaptions.Models;
+
+namespace CoolCaptions.Controllers
+{
+    public class MediaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult New()
+        {
+            return View();
+        }
+
+        public IActionResult MostViewed()
+        {
+            return View();
+        }
+
+        public IActionResult Trending()
+        {
+            return View();
+        }
+    }
+}
